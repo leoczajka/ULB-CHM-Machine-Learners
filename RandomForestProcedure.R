2d6c46f26b9963f7ff0ea1dcc7529c7cea23654a
hhoang27799 <83954672+hhoang27799@users.noreply.github.com>
2021-05-15 16:31:20 +0200

    Added basic feature modification
    In this case installer gets lowercased, slightly improved accuracy in the final model

--- RandomForestProcedure.R
+++ RandomForestProcedure.R
@@ -13,16 +13,25 @@ test_values <- read.csv("testsetvalues.csv")
 # merging the training values and labels, 
 # essentially just adds the training labels
 train <- merge(train_values, train_labels)
+
+# Looking at the installer feature
+summary(as.factor(train$installer))
+# lowercasing installer
+train$altinstaller <- tolower(train$installer)
+# looking at the new alt-installer feature
+summary(as.factor(train$altinstaller))
+
+
 set.seed(69)
 # construction of the forest model
 model_forest <- randomForest(as.factor(status_group) ~ amount_tsh
-                             + gps_height + installer 
-                             + longitude + latitude
+                             + gps_height + altinstaller + date_recorded
+                             + longitude + latitude + management
                              + construction_year + extraction_type_group
-                             + quality_group + quantity + source_class
+                             + water_quality + quantity + source_class
                              + waterpoint_type, 
                              data = train, importance = TRUE,
-                             ntree = 9, nodesize = 3)
+                             ntree = 42, nodesize = 2)
 
 
 # get model statistics
@@ -32,6 +41,9 @@ importance(model_forest)
 forest_pred_train <- predict(model_forest, train)
 # get prediction stats
 confusionMatrix(forest_pred_train, as.factor(train$status_group))
+
+# lowercasing installer in test set
+test_values$altinstaller <- tolower(test_values$installer)
 
 # predicting test set with model
 forest_pred_test <- predict(model_forest, test_values)
@@ -43,4 +55,3 @@ names(submission)[1] <- "id"
 
 # printing submission to csv
 write.csv(submission, file = "submission.csv", row.names = FALSE)
-
